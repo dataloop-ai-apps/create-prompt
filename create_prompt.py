a7271c45a9a3cf0c234fbe9d67bc7a8eef28a0bb
Daniel K. Franch <daniel.f@dataloop.ai>
2024-05-12 12:14:16 +0300

    Better description in manifest

--- create_prompt.py
+++ create_prompt.py
@@ -51,23 +51,29 @@ class ServiceRunner(dl.BaseServiceRunner):
         return output_item
 
     @staticmethod
-    def create_prompt_from_text_annotations(item: dl.Item,
+    def create_prompt_from_item_annotations(item: dl.Item,
                                             context: dl.Context) -> dl.Item:
         node = context.node
         prompt_text = node.metadata['customNodeConfig']["prompt_text"]
         combine_texts = node.metadata['customNodeConfig']["combine_texts"]
         prefix = node.metadata['customNodeConfig']["prefix"]
         directory = node.metadata['customNodeConfig']["directory"]
+        annotation_type = node.metadata['customNodeConfig']['annotation_type']
 
-        text_annotations_filter = dl.Filters('type', 'text', use_defaults=False, resource=dl.FiltersResource.ANNOTATION)
-        text_annotations = item.annotations.list(filter=text_annotations_filter)
-        if len(text_annotations) > 0:
+        annotations_filter = dl.Filters('type', annotation_type, use_defaults=False, resource=dl.FiltersResource.ANNOTATION)
+        annotations = item.annotations.list(filter=annotations_filter)
+        if len(annotations) > 0:
             if prompt_text and combine_texts:
                 text = prompt_text
             else:
                 logger.warning("Failed to combine text item input with prompt. Using just text item input.")
                 text = ''
-            text += " ".join([ann.coordinates for ann in text_annotations])
+            if annotation_type == 'text':
+                text += " ".join([ann.coordinates for ann in annotations])
+            elif annotation_type == 'subtitle':
+                text += " ".join([ann.coordinates['text'] for ann in annotations])
+            else:
+                raise TypeError(f"Annotation of type {annotation_type} is not supported by prompt tools.")
             prompt_name = f"{prefix}-{os.path.splitext(item.name)[0]}"
             prompt_item = dl.PromptItem(name=prompt_name)
             prompt = dl.Prompt(key="prompt_from_annotations")
@@ -83,33 +89,32 @@ class ServiceRunner(dl.BaseServiceRunner):
             raise Exception(f"Item {item.id} has no text annotations")
 
     @staticmethod
-    def create_prompt_from_subtitle_annotations(item: dl.Item,
-                                                context: dl.Context) -> dl.Item:
+    def create_prompt_from_annotation(annotation: dl.Annotation, context: dl.Context) -> dl.Item:
         node = context.node
         prompt_text = node.metadata['customNodeConfig']["prompt_text"]
         combine_texts = node.metadata['customNodeConfig']["combine_texts"]
         prefix = node.metadata['customNodeConfig']["prefix"]
         directory = node.metadata['customNodeConfig']["directory"]
 
-        subtitle_annotations_filter = dl.Filters('type', 'subtitle', resource=dl.FiltersResource.ANNOTATION)
-        subtitle_annotations = item.annotations.list(filters=subtitle_annotations_filter)
-        if len(subtitle_annotations) > 0:
-            if prompt_text and combine_texts:
-                text = prompt_text
-            else:
-                logger.warning("Failed to combine text item input with prompt. Using just text item input.")
-                text = ''
-            text += " ".join([ann.coordinates['text'] for ann in subtitle_annotations])
-            prompt_name = f"{prefix}-{os.path.splitext(item.name)[0]}"
-            prompt_item = dl.PromptItem(name=prompt_name)
-            prompt = dl.Prompt(key="prompt_from_annotations")
-            prompt.add(mimetype=dl.PromptType.TEXT, value=text)
-            prompt_item.add(prompt)
-            output_item = item.dataset.items.upload(prompt_item, overwrite=True, remote_path=directory)
-            logger.info(f"Created prompt item {output_item.id} from input item {item.id} at directory {directory} in "
-                        f"dataset {item.dataset.name}")
-            output_item.metadata["original_item"] = item.metadata.get("original_item", item.id)
-            output_item = output_item.update()
-            return output_item
+        if prompt_text and combine_texts:
+            text = prompt_text
         else:
-            raise Exception(f"Item {item.id} has no subtitle annotations")
+            logger.warning("Failed to combine text item input with prompt. Using just annotation text input.")
+            text = ''
+        if annotation.type == dl.AnnotationType.TEXT:
+            text += f" {annotation.coordinates}"
+        elif annotation.type == dl.AnnotationType.SUBTITLE:
+            text += f" {annotation.coordinates['text']}"
+        else:
+            raise TypeError(f"Annotation of type {annotation.type} is not supported by prompt tools.")
+        prompt_name = f"{prefix}-{os.path.splitext(annotation.item.name)[0]}"
+        prompt_item = dl.PromptItem(name=prompt_name)
+        prompt = dl.Prompt(key="prompt_from_annotations")
+        prompt.add(mimetype=dl.PromptType.TEXT, value=text)
+        prompt_item.add(prompt)
+        output_item = annotation.item.dataset.items.upload(prompt_item, overwrite=True, remote_path=directory)
+        logger.info(f"Created prompt item {output_item.id} from input annotation {annotation.id} at directory {directory} in "
+                    f"dataset {annotation.item.dataset.name}")
+        output_item.metadata["original_item"] = annotation.item.metadata.get("original_item", annotation.item.id)
+        output_item = output_item.update()
+        return output_item
